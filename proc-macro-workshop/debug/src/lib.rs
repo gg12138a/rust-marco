1fe8ce1a5464cbdfddfc6420e25122b77c66529b
gxy <2793392818@qq.com>
2023-06-26 20:07:54 +0800

    Derive macro: `CustomDebug`, step 1 finished.

--- proc-macro-workshop/debug/src/lib.rs
+++ proc-macro-workshop/debug/src/lib.rs
@@ -1,8 +1,18 @@
 use proc_macro::TokenStream;
+use syn::DeriveInput;
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let st = syn::parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    match do_syntax_tree_expand(&st) {
+        Ok(token_stream) => token_stream.into(),
+        Err(e) => e.into_compile_error().into(),
+    }
+}
+
+fn do_syntax_tree_expand(_st: &DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let new_token_stream = proc_macro2::TokenStream::new();
+
+    Ok(new_token_stream)
 }
